871e5e06e3b0594c5dfd5b3f0334b6b3f1a3cb86
User <hyperbolicpotato@protonmail.com>
2024-11-05 19:47:04 +0000

    Reverted to Java 11 and tested html build

--- core/src/main/java/itchminijam/mystic/MysticTarotBattler.java
+++ core/src/main/java/itchminijam/mystic/MysticTarotBattler.java
@@ -2,7 +2,6 @@ package itchminijam.mystic;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
@@ -21,8 +20,12 @@ public class MysticTarotBattler extends ApplicationAdapter {
     @Override
     public void render() {
         ScreenUtils.clear(0.15f, 0.15f, 0.2f, 1f);
+
+        float windowWidth = Gdx.graphics.getWidth();
+        float windowHeight = Gdx.graphics.getHeight();
+
         batch.begin();
-        batch.draw(image, 140, 210);
+        batch.draw(image, windowWidth / 2 - image.getWidth() / 2, windowHeight / 2 - image.getHeight() / 2);
         batch.end();
     }
 
